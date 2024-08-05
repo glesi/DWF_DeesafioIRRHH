214899303e77f3fae4d79d0b1d2f5fe8b79d37ae
glesi <glesi.mayandy@gmail.com>
2024-08-05 15:47:50 -0600

    Se configura proyecto para que sea web, se agregan controladores bases.

--- UDB_RRHH/src/main/java/com/udb/dwf/rrhh/controllers/ContracionesControllers.java
+++ UDB_RRHH/src/main/java/com/udb/dwf/rrhh/controllers/ContracionesControllers.java
@@ -1,4 +1,21 @@
 package com.udb.dwf.rrhh.controllers;
 
-public class ContracionesControllers {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ContracionesControllers extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Se recibiran los valores desde el Frontend
+
+
+
+    }
+
 }
