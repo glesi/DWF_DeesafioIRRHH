9741a6229293694678cc3da63e8a47db025c3889
Stanley43833 <126131991+Stanley43833@users.noreply.github.com>
2024-08-04 19:00:22 -0600

    Create Actualizacion.java

---
+++ UDB_RRHH/src/main/java/com/udb/dwf/rrhh/pojos/Actualizacion.java
@@ -0,0 +1,11 @@
+package com.udb.dwf.rrhh.pojos;
+
+public class Actualizacion {
+    private int idContratacion;
+    private int idDepartamento;
+    private int idEmpleada;
+    private int idCargo;
+    private int idTipoContratacion;
+    private int fechaContratacion;
+    private double salario;
+    private boolean estado;
