871f734cff5a5055fac9f4b629d25a33dd813ea2
jorgeee0-e <gonzalezmenjivar.5@gmail.com>
2024-08-10 22:51:53 -0600

    adding sql sentences to add data to bd

---
+++ data_insert/query_insert_empleados.sql
@@ -0,0 +1,13 @@
+INSERT INTO `udb_rrhh`.`empleados`
+    (`numeroDui`, `nombrePersona`, `usuario`, `numeroTelefono`, `correoInstitucional`, `fechaNacimiento`)
+VALUES
+    ('123456789', 'Ana Martínez', 'anamartinez', '555-1234', 'ana.martinez@empleado.udb.edu.sv', '1985-04-10'),
+    ('234567890', 'Luis García', 'luisgarcia', '555-5678', 'luis.garcia@empleado.udb.edu.sv', '1982-07-15'),
+    ('345678901', 'Carlos López', 'carloslopez', '555-8765', 'carlos.lopez@empleado.udb.edu.sv', '1990-02-20'),
+    ('456789012', 'María Fernández', 'mariafernandez', '555-3456', 'maria.fernandez@empleado.udb.edu.sv', '1988-11-05'),
+    ('567890123', 'José Morales', 'josemorales', '555-7890', 'jose.morales@empleado.udb.edu.sv', '1984-09-30'),
+    ('678901234', 'Laura Pérez', 'lauraperez', '555-6543', 'laura.perez@empleado.udb.edu.sv', '1991-01-12'),
+    ('789012345', 'Miguel Rodríguez', 'miguelrodriguez', '555-4321', 'miguel.rodriguez@empleado.udb.edu.sv', '1987-06-25'),
+    ('890123456', 'Isabel Torres', 'isabeltorres', '555-8765', 'isabel.torres@empleado.udb.edu.sv', '1983-03-14'),
+    ('901234567', 'David Gómez', 'davidgomez', '555-2345', 'david.gomez@empleado.udb.edu.sv', '1992-10-22'),
+    ('012345678', 'Sofía Ramírez', 'sofiaramirez', '555-5678', 'sofia.ramirez@empleado.udb.edu.sv', '1989-12-05');
