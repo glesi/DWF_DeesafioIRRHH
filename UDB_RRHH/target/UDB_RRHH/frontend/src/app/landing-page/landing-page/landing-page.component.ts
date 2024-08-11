1f184ecf2c79878fafb1b63ace4a8d100d3aab2c
glesi <glesi.mayandy@gmail.com>
2024-08-10 22:31:59 -0600

    Se completa crud para la clase Cargo y se solventan errores para tipoContrataciones.

---
+++ UDB_RRHH/target/UDB_RRHH/frontend/src/app/landing-page/landing-page/landing-page.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { TipoContratacionService } from '../../../services/tipoContratacionService/tipoContratacion.service';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+
+  constructor(private tipoContratacionService: TipoContratacionService) {}
+
+  ngOnInit(): void {
+    this.getTipoContrataciones()
+  }
+
+  getTipoContrataciones() {
+    this.tipoContratacionService.get().subscribe({
+      next: (result) => {
+        console.log(result)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+}
