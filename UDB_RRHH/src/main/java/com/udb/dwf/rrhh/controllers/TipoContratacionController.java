5c41bfd934e201affa4c963a8c10d07adf42e0fb
DeboraRivas1805 <00059922@uca.edu.sv>
2024-08-12 21:08:24 -0600

    Conexión entre Frontend y Backend con TipoContratacion

--- UDB_RRHH/src/main/java/com/udb/dwf/rrhh/controllers/TipoContratacionController.java
+++ UDB_RRHH/src/main/java/com/udb/dwf/rrhh/controllers/TipoContratacionController.java
@@ -10,12 +10,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 //Servlet que maneja el CRUD de la Tabla TipoContratacion
