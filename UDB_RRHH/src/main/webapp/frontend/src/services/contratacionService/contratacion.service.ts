0f739fd8a3c05a56c91a5f9f8b747a484c44c926
DeboraRivas1805 <00059922@uca.edu.sv>
2024-08-13 21:03:58 -0600

    Controllers actualizados a su totalidad

---
+++ UDB_RRHH/src/main/webapp/frontend/src/services/contratacionService/contratacion.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContratacionService {
+
+  API_URL : string = 'http://localhost:8080/UDB_RRHH/contrataciones';
+
+  constructor(private httpClient : HttpClient) { }
+
+  get(): Observable<any> {
+    return this.httpClient.get(this.API_URL).pipe(res => res);
+  }
+
+  post(object : any): Observable<any> {
+    return this.httpClient.post(this.API_URL, object).pipe(res => res);
+  }
+
+}
