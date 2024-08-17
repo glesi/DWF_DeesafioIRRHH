e2a6b35f7778fb5907e775be452b973a9267c4f2
jorgeee0-e <gonzalezmenjivar.5@gmail.com>
2024-08-17 10:40:57 -0600

    Creating CRUD-Cargo

---
+++ UDB_RRHH/src/main/webapp/frontend/src/app/CRUD-Cargo/crud-cargo/crud-cargo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-cargo',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-cargo.component.html',
+  styleUrl: './crud-cargo.component.css'
+})
+export class CRUDCargoComponent {
+
+}
